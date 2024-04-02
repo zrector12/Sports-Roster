b5a8cbf3abcdc23eb0f1538407eec442012c0efd
brendanwaters <144367082+brendanwaters@users.noreply.github.com>
2024-04-02 12:36:46 -0400

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,17 @@
+public class Team {
+    private String offensiveStyle;
+    private String deffensiveStyle;
+    private String[] needs;
+
+    public String getOffensiveStyle() {
+        return offensiveStyle;
+    }
+
+    public String getDeffensiveStyle() {
+        return deffensiveStyle;
+    }
+
+    public String[] getNeeds() {
+        return needs;
+    }
+}

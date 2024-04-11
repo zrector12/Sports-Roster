9d8102827efce03036eec966fc467b643d47205c
bmccallu22 <bmccallu22@bw.edu>
2024-04-11 14:53:45 -0400

    Commit again

--- Finalproject/src/QB.java
+++ Finalproject/src/QB.java
@@ -1,12 +1,10 @@
 package Finalproject.src;
 
 class QB extends Player {
-    public QB(String firstName, String lastName, int overallRating) {
-        super(firstName, lastName, "QB", overallRating);
+    public QB(String firstName, String lastName, int overallRating) {super(firstName, lastName, "QB", overallRating);
     }
 
     @Override
-    public int getRankForTeam(Team team) {
-        return (int) (overallRating * team.getPositionPriority("QB"));
+    public int getRankForTeam(Team team) {return (int) (overallRating * team.getPositionPriority("QB"));
     }
 }
